3721ccda3df7c69a3096f72e714c2c5c7d411848
franco.benito <franco.benito@ficohsa.com>
2024-10-25 18:11:14 -0300

    moongose

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const keys = require('./config/keys') // get config keys
 const app = express();
+const moongose = require('mongoose');
 
 require('./services/passport') // execute passport.js
-
 require('./routes/authRoutes')(app); // execute routes and send app module
 
+moongose.connect(keys.mongoURI); // mongoose connection
 
 
 const PORT = process.env.PORT || 5000;
